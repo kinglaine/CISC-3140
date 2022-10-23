88a7e9ba1b338727a2bc441a969ac02288c5d34d
woodley gelin <gelinw3@gmail>
2022-10-23 17:24:02 -0400

    create function

--- JavaProgram/src/part1/RegularExpression.java
+++ JavaProgram/src/part1/RegularExpression.java
@@ -1,10 +1,12 @@
 package part1;
 
 public class RegularExpression {
-
+	public static int dateCalculator() {
+		return 4;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(dateCalculator());
 	}
 
 }
