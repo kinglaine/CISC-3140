8eed4a86c13e68e5418382b23f5bc93c3e3bb101
woodley gelin <gelinw3@gmail>
2022-10-26 10:10:13 -0400

    try fix

--- JavaProgram/src/part1/RegularExpression.java
+++ JavaProgram/src/part1/RegularExpression.java
@@ -3,66 +3,97 @@ import java.util.*;
 public class RegularExpression {
 	public static int dateCalculator(String dateOne, String dateTwo) {
 		//declare dates variables
-		int dateOneMonths;
-		int dateOneDays;
-		int dateOneYears;
+		double dateOneMonths;
+		double dateOneDays;
+		double dateOneYears;
 		
-		int dateTwoMonths;
-		int dateTwoDays;
-		int dateTwoYears;
+		double dateTwoMonths;
+		double dateTwoDays;
+		double dateTwoYears;
+		String format1 = "\\d{2}-\\d{2}"; String format2 = "\\d{2}\\/\\d{2}\\/\\d{4}";
 		
 		//check input format 
-		if(dateOne.matches("\\d{2}-\\d{2}") && dateTwo.matches("\\d{2}\\/\\d{2}\\/\\d{4}")) {
+		if(dateOne.matches(format1) && dateTwo.matches(format2)) {
 			//get the values for first date
-			dateOneMonths = Integer.parseInt(dateOne.substring(0,2));
-			dateOneDays = Integer.parseInt(dateOne.substring(3,5));
+			dateOneMonths = Double.parseDouble(dateOne.substring(0,2));
+			dateOneDays = Double.parseDouble(dateOne.substring(3,5));
 			dateOneYears = 2023;
 			
 			//get the values for second date
-			dateTwoMonths = Integer.parseInt(dateTwo.substring(0,2));
-			dateTwoDays = Integer.parseInt(dateTwo.substring(3,5));
-			dateTwoYears = Integer.parseInt(dateTwo.substring(6,10));
+			dateTwoMonths = Double.parseDouble(dateTwo.substring(0,2));
+			dateTwoDays = Double.parseDouble(dateTwo.substring(3,5));
+			dateTwoYears = Double.parseDouble(dateTwo.substring(6,10));
 				
-		}else if(dateOne.matches("\\d{2}\\/\\d{2}\\/\\d{4}") && dateTwo.matches("\\d{2}-\\d{2}")) {
+		}else if(dateOne.matches(format2) && dateTwo.matches(format1)) {
 			//get the values for first date
-			dateOneMonths = Integer.parseInt(dateOne.substring(0,2));
-			dateOneDays = Integer.parseInt(dateOne.substring(3,5));
-			dateOneYears = Integer.parseInt(dateOne.substring(6,10));
+			dateOneMonths = Double.parseDouble(dateOne.substring(0,2));
+			dateOneDays = Double.parseDouble(dateOne.substring(3,5));
+			dateOneYears = Double.parseDouble(dateOne.substring(6,10));
 			
 			//get the values for second date
-			dateTwoMonths = Integer.parseInt(dateTwo.substring(0,2));
-			dateTwoDays = Integer.parseInt(dateTwo.substring(3,5));
+			dateTwoMonths = Double.parseDouble(dateTwo.substring(0,2));
+			dateTwoDays = Double.parseDouble(dateTwo.substring(3,5));
 			dateTwoYears = 2023;
-		} else if(dateOne.matches("\\d{2}-\\d{2}") && dateTwo.matches("\\d{2}-\\d{2}")) {
+		} else if(dateOne.matches(format1) && dateTwo.matches(format1)) {
 			//get the values for first date
-			dateOneMonths = Integer.parseInt(dateOne.substring(0,2));
-			dateOneDays = Integer.parseInt(dateOne.substring(3,5));
+			dateOneMonths = Double.parseDouble(dateOne.substring(0,2));
+			dateOneDays = Double.parseDouble(dateOne.substring(3,5));
 			dateOneYears = 2023;
 			
 			//get the values for second date
-			dateTwoMonths = Integer.parseInt(dateTwo.substring(0,2));
-			dateTwoDays = Integer.parseInt(dateTwo.substring(3,5));
+			dateTwoMonths = Double.parseDouble(dateTwo.substring(0,2));
+			dateTwoDays = Double.parseDouble(dateTwo.substring(3,5));
 			dateTwoYears = 2023;
 		}else {
 			//get the values for first date
-			dateOneMonths = Integer.parseInt(dateOne.substring(0,2));
-			dateOneDays = Integer.parseInt(dateOne.substring(3,5));
-			dateOneYears = Integer.parseInt(dateOne.substring(6,10));
+			dateOneMonths =Double.parseDouble(dateOne.substring(0,2));
+			dateOneDays = Double.parseDouble(dateOne.substring(3,5));
+			dateOneYears = Double.parseDouble(dateOne.substring(6,10));
 			
 			//get the values for second date
-			dateTwoMonths = Integer.parseInt(dateTwo.substring(0,2));
-			dateTwoDays = Integer.parseInt(dateTwo.substring(3,5));
-			dateTwoYears = Integer.parseInt(dateTwo.substring(6,10));
+			dateTwoMonths = Double.parseDouble(dateTwo.substring(0,2));
+			dateTwoDays =Double.parseDouble(dateTwo.substring(3,5));
+			dateTwoYears = Double.parseDouble(dateTwo.substring(6,10));
 			
 		}
 		//subtract monthsOne from monthTwo to get how many months left then convert to days
-		int daysInMonths = (Math.abs(dateTwoMonths - dateOneMonths)/12) * 365;
-		int daysInDays = Math.abs(dateTwoDays - dateOneDays);
-		int daysInYears  = Math.abs(dateTwoYears - dateOneYears) * 365;
+		double daysInMonths = (Math.abs(dateTwoMonths - dateOneMonths)/12) * 365;
+		double daysInDays = Math.abs(dateTwoDays - dateOneDays);
+		double daysInYears  = Math.abs(dateTwoYears - dateOneYears) * 365;
 		
-		int days = daysInMonths + daysInDays + daysInYears;
+		int days = (int)(daysInMonths + daysInDays + daysInYears);
 		
 		return days;
+	}
+  	public static String getDateOne(String dates) {
+  		String dateOne;
+  		String format1 = "\\d{2}-\\d{2}"; String format2 = "\\d{2}\\/\\d{2}\\/\\d{4}";
+		//check input format 
+		if(dates.matches(format1 + " " + format2)) {
+			dateOne = dates.substring(0,6);	
+		}else if(dates.matches(format2 + " " + format1)) {
+			dateOne = dates.substring(0,10);
+		} else if(dates.matches(format1 + " " + format1)) {
+			dateOne = dates.substring(0,5);
+		}else {
+			dateOne = dates.substring(0,10);
+		}
+		return dateOne;
+	}
+	public static String getDateTwo(String dates) {
+		String dateTwo;
+		String format1 = "\\d{2}-\\d{2}"; String format2 = "\\d{2}\\/\\d{2}\\/\\d{4}";
+		//check input format 
+		if(dates.matches(format1 + " " + format2)) {
+			dateTwo = dates.substring(6,16);	
+		}else if(dates.matches(format2 + " " + format1)) {
+			dateTwo = dates.substring(11,16);
+		} else if(dates.matches(format1 + " " + format1)) {
+			dateTwo = dates.substring(6,11);
+		}else {
+			dateTwo = dates.substring(11,21);
+		}
+		return dateTwo;
 	}
 	public static void main(String[] args) {
 		// allow user to select between available saved dates
@@ -76,9 +107,10 @@ public class RegularExpression {
 		System.out.println("Type AQ then Enter to get days between assignment and quiz date");
 		System.out.println("Type AF then Enter to get days between assignment and final date");
 		System.out.println("Type QF then Enter to get days between quiz  and final");
-		System.out.println("Type anything to Enter your own dates");
+		System.out.println("Type I to Enter your own dates");
 		
-			switch(input.next()) {
+		String in = input.next();
+			switch(in) {
 				case "AQ":
 					System.out.println("There are " + dateCalculator(assignmentDueDate, quizDate) + " days between the assignment and the quiz.");
 					break;
@@ -88,23 +120,25 @@ public class RegularExpression {
 				case "QF":
 					System.out.println("There are " + dateCalculator(finalDate, quizDate) + " days between the quiz and the final.");
 					break;
-				default:
-					while(true) {
-						System.out.print("Enter first date in MM/DD/YYY or MM-DD format: ");
-						String dateOne = input.next();
-						if(!dateOne.matches("\\d{2}-\\d{2}") && !dateOne.matches("\\d{2}\\/\\d{2}\\/\\d{4}")) {
-							System.out.println("Wrong input!");
+				case "I":
+					try {
+						while(true) {
+							System.out.print("Enter two dates in MM/DD/YYY or MM-DD format seperate by space:");
+							String dates = input.next() + " " +  input.next();
+							if((getDateOne(dates).matches("\\d{2}-\\d{2}") || getDateOne(dates).matches("\\d{2}\\/\\d{2}\\/\\d{4}"))
+							&& getDateTwo(dates).matches("\\d{2}-\\d{2}") || getDateTwo(dates).matches("\\d{2}\\/\\d{2}\\/\\d{4}")) {
+								System.out.println("There are " + dateCalculator(getDateOne(dates),getDateTwo(dates)) + " days between " + 
+										getDateOne(dates) + " and "  + getDateTwo(dates));
+							}
+							
 						}
-						System.out.print("Enter second date in MM/DD/YYY or MM-DD format: ");
-						String dateTwo = input.next();
-						if(!dateTwo.matches("\\d{2}-\\d{2}") && !dateTwo.matches("\\d{2}\\/\\d{2}\\/\\d{4}")) {
-							System.out.println("Wrong input!");
-						}else {
-							System.out.println("There are " + dateCalculator(dateOne,dateTwo) + " days bewteen " + dateOne + " and " + dateTwo + "\n");
-						}
-						
+					}catch(Exception e) {
+						System.out.println(" wrong input!");
 					}
+					
 			}
+		
+		
 	}
 
 }
